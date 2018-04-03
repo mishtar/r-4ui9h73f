8d1aaecfaf64e45f19455a474bed32af6bc3bd66
Sebastian Arboleda <SArboleda@arrivelogistics.com>
2018-04-02 19:26:51 -0500

    exercise

--- src/App.js
+++ src/App.js
@@ -2,14 +2,25 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      value: ''
+    };
+    this.repetir = this.repetir.bind(this);
+  }
   render() {
     return (
       <div>
         <h1>Un repetidor</h1>
-        <input type="text" placeholder="Empieza a escribir algo" />
-        <p className="repeater"></p>
+        <input type="text" placeholder="Empieza a escribir algo" onChange={this.repetir} />
+        <p className="repeater">{this.state.value}</p>
       </div>
     );
+  }
+
+  repetir(event){
+    this.setState({value: event.target.value});
   }
 }
 
